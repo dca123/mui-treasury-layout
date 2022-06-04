a3e6a7c72b97f981bb2c668850dbe55c746b1bf9
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2022-06-04 19:28:08 +0530

    with latest mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Root,
+  Header,
+  EdgeSidebar,
+  EdgeTrigger,
+  SidebarContent,
+  Content,
+  Footer,
+} from "@mui-treasury/layout";
+import Box from "@mui/material/Box";
+import ButtonBase from "@mui/material/ButtonBase";
+import CssBaseline from "@mui/material/CssBaseline";
+import IconButton from "@mui/material/IconButton";
 
-function App() {
+import Menu from "@mui/icons-material/Menu";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Root
+      scheme={{
+        header: {
+          config: {
+            xs: {
+              position: "sticky",
+              height: 56,
+            },
+            md: {
+              position: "relative",
+              height: 64,
+            },
+          },
+        },
+        leftEdgeSidebar: {
+          config: {
+            xs: {
+              variant: "temporary",
+              width: "auto",
+            },
+            md: {
+              variant: "permanent",
+              width: 256,
+              collapsible: true,
+              collapsedWidth: 64,
+            },
+          },
+        },
+      }}
+    >
+      <CssBaseline />
+      <Header>
+        <Box
+          sx={{ flex: 1, display: "flex", alignItems: "center", px: 2, gap: 1 }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <EdgeTrigger target={{ anchor: "left", field: "open" }}>
+            {(open: any, setOpen: (arg0: boolean) => void) => (
+              <IconButton onClick={() => setOpen(!open)}>
+                {open ? <KeyboardArrowLeft /> : <Menu />}
+              </IconButton>
+            )}
+          </EdgeTrigger>
+          Header
+        </Box>
+      </Header>
+      <EdgeSidebar anchor="left">
+        <SidebarContent>Sidebar content</SidebarContent>
+        <EdgeTrigger target={{ anchor: "left", field: "collapsed" }}>
+          {(collapsed: any, setCollapsed: (arg0: boolean) => void) => (
+            <ButtonBase
+              onClick={() => setCollapsed(!collapsed)}
+              sx={{
+                minHeight: 40,
+                width: "100%",
+                bgcolor: "grey.100",
+                borderTop: "1px solid",
+                borderColor: "grey.200",
+              }}
+            >
+              {collapsed ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+            </ButtonBase>
+          )}
+        </EdgeTrigger>
+      </EdgeSidebar>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+    </Root>
   );
-}
+};
 
 export default App;
